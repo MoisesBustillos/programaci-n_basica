2ee060c56fdf5864e2161e501e0d67b1fbea9591
MoisesBustillos <a361352@uach.mx>
2024-05-02 08:11:52 -0600

    Se agrega el archivo 7, Introducir tantas frases como queramos y contarlas.

---
+++ 7.js
@@ -0,0 +1,7 @@
+// Ejercicio 7
+let countImpares2 = 0;
+for (let i = 1; i <= 100; i += 2) {
+    console.log(i);
+    countImpares2++;
+}
+console.log(`Hay ${countImpares2} números impares hasta el 100.`);
