e407fd92cd120602cc03c79bb0870ad7ed661412
MoisesBustillos <a361352@uach.mx>
2024-05-02 08:25:39 -0600

    Se agrega el archivo 32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,3 @@
+// Ejercicio 32
+const nombres = ["Ana", "Beatriz", "Carlos", "David", "Elena", "Fernando", "Gloria", "Héctor", "Irene", "Juan", "Karla", "Luis", "María", "Nicolás", "Olivia", "Pablo", "Quinta", "Rosa", "Santiago", "Teresa"];
+nombres.forEach(nombre => console.log(nombre));
