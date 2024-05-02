e40287e265ca3f7a6cb352d854621e586f97eaa3
MoisesBustillos <a361352@uach.mx>
2024-05-02 08:17:23 -0600

    Se agrega el archivo 16.- Imprimir diez veces la serie de números del 1 al 10.

---
+++ 16.js
@@ -0,0 +1,10 @@
+// Ejercio 16
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Serie ${i + 1}:`);
+    for (let j = 1; j <= 10; j++) {
+      console.log(j);
+    }
+    console.log(""); // Añade una línea en blanco entre cada serie
+  }
+  
