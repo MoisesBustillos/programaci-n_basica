202652018c8c5da55fb63fdf791dc5840ea9abc5
MoisesBustillos <a361352@uach.mx>
2024-05-02 08:09:16 -0600

    Se agrega el archivo 5, Hacer un programa que imprima los números impares hasta el 100 y que imprima cuantos impares hay.

---
+++ 5.js
@@ -0,0 +1,7 @@
+// Ejercicio 5
+let countImpares = 0;
+for (let i = 1; i <= 100; i += 2) {
+    console.log(i);
+    countImpares++;
+}
+console.log(`Hay ${countImpares} números impares hasta el 100.`);
